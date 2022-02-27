f7c22747f41d773aee9091a12d04f2b6b9eaf1f9
Kedar Chandulur <47148900+KedarChandulur@users.noreply.github.com>
2022-02-27 23:23:01 +0530

    Added SetVec3 func in Shader class.

--- src/Shader.h
+++ src/Shader.h
@@ -27,6 +27,8 @@ public:
 	void SetUniform1i(const std::string& name, int value);
 	void SetUniform1f(const std::string& name, float value);
 	void SetUniform4f(const std::string& name, float v0, float v1, float f2, float f3);
+	void SetVec3(const std::string& name, float v0, float v1, float v2);
+	void SetVec3(const std::string& name, float value);
 
 	void SetUniformMat4f(const std::string& name, const glm::mat4& matrix);
 	void SetUniformMat4fp(const std::string& name, const float* value);
