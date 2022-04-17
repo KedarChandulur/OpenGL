cfdbba94c93f908e03b9fb046f400f40d3db3f95
Kedar Chandulur <47148900+KedarChandulur@users.noreply.github.com>
2022-04-17 12:59:21 +0530

    Added test int main with RUNMAIN macro.

--- src/Application.cpp
+++ src/Application.cpp
@@ -6,12 +6,21 @@
 #include "Cursor.h"
 #include "Texture.h"
 
+#include "Model.h"
+//#include "Model_Testing_Class.h"
+
 #include "glm/gtc/matrix_transform.hpp"
 #include "glm/gtc/type_ptr.hpp"
 
 #include "imgui/imgui.h"
 #include "imgui/imgui_impl_glfw.h"
 #include "imgui/imgui_impl_opengl3.h"
+
+#define RUNMAIN 0
+
+#if !RUNMAIN
+#include <stb_image/stb_image.h>
+#endif
 
 #pragma region CallBackBridge_Method
 //Reference taken from: https://github.com/glfw/glfw/issues/815#issuecomment-235986227
@@ -21,6 +30,7 @@
 //};
 #pragma endregion
 
+#if RUNMAIN
 int main(void)
 {
     GLFWwindow* window;
@@ -68,6 +78,8 @@ int main(void)
     callBackBridge.SetMouseCallbacks(window, camera);
 
     Cursor cursor(window, true);
+
+    Model backpackModel("res/Models/backpack.obj");
 
     //glm::vec3 lightPos(1.25f, 1.5f, 2.0f);
     //glm::vec3 lightPos(0.8f, 0.0f, 0.5f);
@@ -324,6 +336,8 @@ int main(void)
             glm::mat4 model_L = glm::mat4(1.0f);
             objectShader.SetUniformMat4fp("model", glm::value_ptr(model_L));
 
+            backpackModel.Draw(objectShader);
+
             lightShader.Bind();
             lightShader.SetUniformMat4f("projection", projection);
             lightShader.SetUniformMat4f("view", view);
@@ -360,3 +374,112 @@ int main(void)
     glfwTerminate();
     return 0;
 }
+#else
+int main(void)
+{
+    GLFWwindow* window;
+
+    if (!glfwInit())
+        return -1;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    const uint32_t screenWidth = 640;
+    const uint32_t screenHeight = 480;
+
+    window = glfwCreateWindow(screenWidth, screenHeight, "OpenGL Window", NULL, NULL);
+
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    glfwSwapInterval(1);
+
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "Error!" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    std::cout << "GL_VERSION: " << glGetString(GL_VERSION) << std::endl;
+
+    GLCall(glViewport(0, 0, screenWidth, screenHeight));
+
+    Camera camera(glm::vec3(0.0f, 0.0f, 6.0f));
+    camera.settings.aspectRatio = (float)screenWidth / (float)screenHeight;
+    camera.mouseMoveData.lastPositions = glm::vec2((float)(screenWidth / 2), (float)(screenHeight / 2));
+
+    CallBackBridge callBackBridge;
+    //glfwSetWindowUserPointer(window, &callBackBridge);
+    callBackBridge.SetWindowResizeCallback(window);
+    callBackBridge.SetMouseCallbacks(window, camera);
+
+    Cursor cursor(window, false);
+
+    stbi_set_flip_vertically_on_load(true);
+
+    Model backpackModel("res/Models/backpack.obj");
+    
+    //Adding extra scope for clean up.
+    {
+        //Texture::SetBlendFunction(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        glEnable(GL_DEPTH_TEST);
+
+        Shader modelShader("res/Shaders/Model.shader");
+        modelShader.Bind();
+
+        modelShader.UnBind();
+
+        glm::mat4 model(1.0f), view(model), projection(model);
+
+        while (!glfwWindowShouldClose(window))
+        {
+            Time::Update();
+
+            Renderer::ProcessInput(window);
+            camera.ProcessInput(window);
+
+            Renderer::Clear();
+
+            modelShader.Bind();
+
+            //Spawning 10 cubes for testing purpose.
+            for (unsigned int i = 0; i < 10; i++)
+            {
+                glm::mat4 model = glm::mat4(1.0f);
+                //model = glm::translate(model, cubePositions[i]);
+                float angle = 20.0f * i;
+                model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+                //objectShader.SetUniformMat4fp("model", glm::value_ptr(model));
+                modelShader.SetUniformMat4f("model", model);
+
+                glDrawArrays(GL_TRIANGLES, 0, 36);
+            }
+
+            //Updating projection matrix again because fov is being changed through mouse scroll.
+            projection = glm::perspective(glm::radians(camera.settings.fov), camera.settings.aspectRatio, camera.settings.near, camera.settings.far);
+            view = camera.GetViewMatrix();
+            modelShader.SetUniformMat4f("projection", projection);
+            modelShader.SetUniformMat4f("view", view);
+            // world transformation
+            glm::mat4 model_L = glm::mat4(1.0f);
+            modelShader.SetUniformMat4fp("model", glm::value_ptr(model_L));
+
+            backpackModel.Draw(modelShader);
+
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+        }
+    }
+
+    glfwTerminate();
+    return 0;
+}
+#endif
