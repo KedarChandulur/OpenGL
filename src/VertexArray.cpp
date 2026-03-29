ad2b51b1fbd6d5ebebc9be262d605c4c2015a45a
Kedar Chandulur <kedarchandulur@gmail.com>
2026-03-29 21:53:05 +0530

    warning fix for the vertex array.

--- src/VertexArray.cpp
+++ src/VertexArray.cpp
@@ -24,6 +24,10 @@ void VertexArray::AddBuffer(const VertexBuffer& vb, const VertexBufferLayout& la
 		GLCall(glEnableVertexAttribArray(i));
 		GLCall(glVertexAttribPointer(i, element.count, element.type, 
 			element.normalized, layout.GetStride(), (const void*)offset));
+
+		//GLCall(glVertexAttribPointer(i, element.count, element.type,
+		//	element.normalized, layout.GetStride(), reinterpret_cast<const void*>(offset)));
+
 		offset += element.count * VertexBufferElement::GetSizeOfType(element.type);
 	}
 }
